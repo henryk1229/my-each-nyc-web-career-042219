2d93566940e1717277ec6725b777aee48baa1ac2
Henry Koehler <henryk1229@gmail.com>
2019-04-04 16:56:46 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,8 @@
 def my_each(array)
   i = 0 
   new_array = []
-  while i < array.length do
-    new_array << "#{array[i]}"
+  while i < array.length
+    yield new_array << "#{array[i]}"
     i += 1 
   end
   new_array
